fbc33bc2dd66c6db2f8d2da06c0979615ace6787
mk1192 <mohankumar.b@outlook.com>
2025-11-29 20:08:28 +0530

    Update importSTGdata.py

--- importSTGdata.py
+++ importSTGdata.py
@@ -2,8 +2,8 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 username = 'testusr'
-password = "Inceptez%40123"#rather than @ i am mentioning %40
-host = '34.63.107.222'
+password = "Changed%40123"#rather than @ i am mentioning %40
+host = ''
 db = 'stgdb_bfsi'
 
 engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}:3306/{db}")
@@ -23,3 +23,4 @@ for table, file in table_list_dict.items():
     df = pd.read_csv(file)
     df.to_sql(table, con=engine, index=False, if_exists="replace")
     print(f"Rows loaded in the table {table}")
+
